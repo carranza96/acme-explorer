217a80fc7dc9770887fa430f64cfd00ad4dae02b
manuel <manuelcarranza96@hotmail.com>
2019-02-12 20:04:01 +0100

    b

---
+++ api/models/applicationModel.js
@@ -0,0 +1,37 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var applicationSchema = new Schema({
+    moment:{
+        type: Date,
+        required: 'Kindly enter the moment',
+        default: Date.now
+        // TODO: Validar restricción fecha pasada
+    },
+    status:{
+        type: String,
+        required: 'Kindly enter the status',
+        default: 'PENDING',
+        enum: ['PENDING','REJECTED', 'DUE','ACCEPTED','CANCELLED'],
+    },
+    comments: [String],
+    rejectReason:{
+        type: String
+    },
+    paid:{
+        type:Boolean,
+        default:false
+    },
+    explorer:{
+        type: Schema.Types.ObjectId,
+        ref: 'Actor',
+        required: 'Kindly enter the explorer id'
+    },
+    trip:{
+        type: Schema.Types.ObjectId,
+        ref: 'Trip',
+        required: 'Kindly enter the trip id' 
+    }
+},  { strict: false })
