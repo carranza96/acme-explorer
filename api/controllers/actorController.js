d268f3afea59655cd957bab8d67da81b997fbabe
manuel <manuelcarranza96@hotmail.com>
2019-02-14 17:08:20 +0100

    jsonprueba added

--- api/controllers/actorController.js
+++ api/controllers/actorController.js
@@ -92,7 +92,7 @@ exports.delete_an_actor = function(req, res) {
 };
 
 exports.delete_all_actors = function(req, res) {
-    Actor.delete({}, function(err, actor) {
+    Actor.remove({}, function(err, actor) {
         if (err){
             res.send(err);
         }
