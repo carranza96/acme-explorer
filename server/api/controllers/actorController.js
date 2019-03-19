e6790d8c44f4e6643643fb10717fe27baf8cf990
Michael Stevan <miestgo@gmail.com>
2019-03-19 14:53:52 +0100

    Checked and formatted actor.

--- server/api/controllers/actorController.js
+++ server/api/controllers/actorController.js
@@ -2,90 +2,88 @@
 /*---------------ACTOR----------------------*/
 var mongoose = require('mongoose'),
   Actor = mongoose.model('Actor');
-  var admin = require('firebase-admin');
-  var authController = require('./authController');
+var admin = require('firebase-admin');
+var authController = require('./authController');
 
-exports.list_all_actors = function(req, res) {
-    //Check if the role param exist
-    var match = {};
-    if(req.query.role){
-        var roleName=req.query.role;
-        match = {role:roleName};
+exports.list_all_actors = function (req, res) {
+  //Check if the role param exist
+  var match = {};
+  if (req.query.role) {
+    var roleName = req.query.role;
+    match = { role: roleName };
+  }
+  Actor.find(match, function (err, actors) {
+    if (err) {
+      res.send(err);
     }
-    Actor.find(match, function(err, actors) {
-        if (err){
-          res.send(err);
-        }
-        else{
-            res.json(actors);
-        }
-    });
+    else {
+      res.json(actors);
+    }
+  });
 };
 
-
-
-exports.create_an_actor = function(req, res) {
+exports.create_an_actor = function (req, res) {
   var new_actor = new Actor(req.body);
-  if(new_actor.role.includes('ADMINISTRATOR') || new_actor.role.includes('MANAGER')){
+  if (new_actor.role.includes('ADMINISTRATOR') || new_actor.role.includes('MANAGER')) {
     res.status(422).send('You cannot create a new administrator/manager');
   }
-  else{
-    new_actor.save(function(err, actor) {
-      if (err){
-        if(err.name=='ValidationError') {
-            res.status(422).send(err);
+  else {
+    new_actor.save(function (err, actor) {
+      if (err) {
+        if (err.name == 'ValidationError') {
+          res.status(422).send(err);
         }
-        else{
+        else {
           res.status(500).send(err);
         }
       }
-      else{
+      else {
         res.json(actor);
       }
     });
   }
 };
-exports.create_a_manager = function(req, res) {
+exports.create_a_manager = function (req, res) {
   var new_actor = new Actor(req.body);
-  if(new_actor.role.includes('ADMINISTRATOR')){
+  if (new_actor.role.includes('ADMINISTRATOR')) {
     res.status(422).send('You cannot create a new administrator');
   }
-  if(!new_actor.role.includes('MANAGER')){
+  if (!new_actor.role.includes('MANAGER')) {
     res.status(422).send('Actor must be a manager');
   }
-  else{
-    new_actor.save(function(err, actor) {
-      if (err){
-        if(err.name=='ValidationError') {
-            res.status(422).send(err);
+  else {
+    new_actor.save(function (err, actor) {
+      if (err) {
+        if (err.name == 'ValidationError') {
+          res.status(422).send(err);
         }
-        else{
+        else {
           res.status(500).send(err);
         }
       }
-      else{
+      else {
         res.json(actor);
       }
     });
   }
 };
 
-exports.create_an_admin = function(req, res) {
+exports.create_an_admin = function (req, res) {
   var new_actor = new Actor(req.body);
-  if(!new_actor.role.includes('ADMINISTRATOR')){
+  if (!new_actor.role.includes('ADMINISTRATOR')) {
     res.status(422).send('Actor must be an administrator');
   }
-  else{
-    new_actor.save(function(err, actor) {
-      if (err){
-        if(err.name=='ValidationError') {
-            res.status(422).send(err);
+  else {
+    new_actor.save(function (err, actor) {
+      if (err) {
+        if (err.name == 'ValidationError') {
+          res.status(422).send(err);
         }
-        else{
+        else {
           res.status(500).send(err);
         }
       }
-      else{
+      else {
         res.json(actor);
       }
     });
@@ -94,85 +92,85 @@ exports.create_an_admin = function(req, res) {
 };
 
 
-exports.login_an_actor = async function(req, res) {
+exports.login_an_actor = async function (req, res) {
   console.log('starting login an actor');
   var emailParam = req.query.email;
   var password = req.query.password;
   Actor.findOne({ email: emailParam }, function (err, actor) {
-      if (err) { res.send(err); }
+    if (err) { res.send(err); }
 
-      // No actor found with that email as username
-      else if (!actor) {
-        res.status(401); //an access token isn’t provided, or is invalid
-        res.json({message: 'forbidden',error: err});
-      }
+    // No actor found with that email as username
+    else if (!actor) {
+      res.status(401); //an access token isn’t provided, or is invalid
+      res.json({ message: 'forbidden', error: err });
+    }
 
-      else if ((actor.banned)) {
-        res.status(403); //an access token is valid, but requires more privileges
-        res.json({message: 'banned actor',error: err});
-      }
-      else{
-        // Make sure the password is correct
-        //console.log('En actor Controller pass: '+password);
-        actor.verifyPassword(password, async function(err, isMatch) {
-          if (err) {
-            res.send(err);
+    else if ((actor.banned)) {
+      res.status(403); //an access token is valid, but requires more privileges
+      res.json({ message: 'banned actor', error: err });
+    }
+    else {
+      // Make sure the password is correct
+      //console.log('En actor Controller pass: '+password);
+      actor.verifyPassword(password, async function (err, isMatch) {
+        if (err) {
+          res.send(err);
+        }
+
+        // Password did not match
+        else if (!isMatch) {
+          //res.send(err);
+          res.status(401); //an access token isn’t provided, or is invalid
+          res.json({ message: 'forbidden', error: err });
+        }
+
+        else {
+          try {
+            var customToken = await admin.auth().createCustomToken(actor.email);
+          } catch (error) {
+            console.log("Error creating custom token:", error);
           }
-
-          // Password did not match
-          else if (!isMatch) {
-            //res.send(err);
-            res.status(401); //an access token isn’t provided, or is invalid
-            res.json({message: 'forbidden',error: err});
-          }
-
-          else {
-              try{
-                var customToken = await admin.auth().createCustomToken(actor.email);
-              } catch (error){
-                console.log("Error creating custom token:", error);
-              }
-              actor.customToken = customToken;
-              console.log('Login Success... sending JSON with custom token');
-              res.json(actor);
-          }
+          actor.customToken = customToken;
+          console.log('Login Success... sending JSON with custom token');
+          res.json(actor);
+        }
       });
     }
   });
 };
 
 
-exports.read_an_actor = function(req, res) {
-  Actor.findById(req.params.actorId, function(err, actor) {
-    if (err){
+exports.read_an_actor = function (req, res) {
+  Actor.findById(req.params.actorId, function (err, actor) {
+    if (err) {
       res.send(err);
     }
-    else{
+    else {
       res.json(actor);
     }
   });
 };
 
-exports.read_an_actor_v2 = function(req, res) {
-  //Managers,Explorers and Sponsors can read theirselves, administrators can read any actor
-  Actor.findById(req.params.actorId, async function(err, actor) {
-    if (err){
+exports.read_an_actor_v2 = function (req, res) {
+  // Managers,Explorers and Sponsors can read theirselves, administrators can read any actor
+  Actor.findById(req.params.actorId, async function (err, actor) {
+    if (err) {
       res.send(err);
     }
-    else if (!actor){
+    else if (!actor) {
       return res.status(404).send(`Actor with id ${req.params.actorId} does not exist in database`);
     }
-    else{
-      if (actor.role.includes('ADMINISTRATOR')){
+    else {
+      if (actor.role.includes('ADMINISTRATOR')) {
         res.json(actor);
       }
-      else if ( actor.role.includes('MANAGER') || actor.role.includes('EXPLORER') || actor.role.includes('SPONSOR')){
+      else if (actor.role.includes('MANAGER') || actor.role.includes('EXPLORER') || actor.role.includes('SPONSOR')) {
         var idToken = req.headers['idtoken'];
         var authenticatedUserId = await authController.getUserId(idToken);
-        if (authenticatedUserId == req.params.actorId){
+        if (authenticatedUserId == req.params.actorId) {
           res.json(actor)
         }
-        else{
+        else {
           res.status(403); //Auth error
           res.send('The Actor is trying to read an Actor that is not himself!');
         }
@@ -185,59 +183,59 @@ exports.read_an_actor_v2 = function(req, res) {
   });
 };
 
-exports.update_an_actor = function(req, res) {
-    Actor.findOneAndUpdate({_id: req.params.actorId}, req.body, {new: true, runValidators:true}, function(err, actor) {
-      if (err){
-        if(err.name=='ValidationError') {
-            res.status(422).send(err);
-        }
-        else{
-          res.status(500).send(err);
-        }
+exports.update_an_actor = function (req, res) {
+  Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true, runValidators: true }, function (err, actor) {
+    if (err) {
+      if (err.name == 'ValidationError') {
+        res.status(422).send(err);
       }
-      else{
-          res.json(actor);
+      else {
+        res.status(500).send(err);
       }
-    });
+    }
+    else {
+      res.json(actor);
+    }
+  });
 };
 
 
-exports.update_an_actor_v2 = function(req, res) {
+exports.update_an_actor_v2 = function (req, res) {
   //Managers,Explorers and Sponsors can update theirselves, administrators can update any actor
-  Actor.findById(req.params.actorId, async function(err, actor) {
-    if (err){
+  Actor.findById(req.params.actorId, async function (err, actor) {
+    if (err) {
       res.send(err);
     }
-    else if (!actor){
+    else if (!actor) {
       return res.status(404).send(`Actor with id ${req.params.actorId} does not exist in database`);
     }
-    else{
-      console.log('actor: '+actor);
+    else {
+      console.log('actor: ' + actor);
       var idToken = req.headers['idtoken'];//WE NEED the FireBase custom token in the req.header['idToken']... it is created by FireBase!!
-      if (actor.role.includes('MANAGER') || actor.role.includes('EXPLORER') || actor.role.includes('SPONSOR') ){
+      if (actor.role.includes('MANAGER') || actor.role.includes('EXPLORER') || actor.role.includes('SPONSOR')) {
         var authenticatedUserId = await authController.getUserId(idToken);
-        if (authenticatedUserId == req.params.actorId){
-          Actor.findOneAndUpdate({_id: req.params.actorId}, req.body, {new: true, runValidators:true}, function(err, actor) {
-            if (err){
+        if (authenticatedUserId == req.params.actorId) {
+          Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true, runValidators: true }, function (err, actor) {
+            if (err) {
               res.send(err);
             }
-            else{
+            else {
               res.json(actor);
             }
           });
-        } else{
+        } else {
           res.status(403); //Auth error
           res.send('The Actor is trying to update an Actor that is not himself!');
         }
-      } else if (actor.role.includes('ADMINISTRATOR')){
-          Actor.findOneAndUpdate({_id: req.params.actorId}, req.body, {new: true, runValidators:true}, function(err, actor) {
-            if (err){
-              res.send(err);
-            }
-            else{
-              res.json(actor);
-            }
-          });
+      } else if (actor.role.includes('ADMINISTRATOR')) {
+        Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true, runValidators: true }, function (err, actor) {
+          if (err) {
+            res.send(err);
+          }
+          else {
+            res.json(actor);
+          }
+        });
       } else {
         res.status(405); //Not allowed
         res.send('The Actor has unidentified roles');
@@ -248,51 +246,51 @@ exports.update_an_actor_v2 = function(req, res) {
 };
 
 
-exports.ban_an_actor = function(req, res) {
-    //Check that the user is an Administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
-    console.log("banning an actor with id: "+req.params.actorId)
-    Actor.findOneAndUpdate({_id: req.params.actorId},  { $set: {"banned": "true" }}, {new: true, runValidators:true}, function(err, actor) {
-      if (err){
-        res.send(err);
-      }
-      else{
-        res.json(actor);
-      }
-    });
-  };
-
-
-exports.unban_an_actor = function(req, res) {
-    //Check that the user is an Administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
-    console.log("banning an actor with id: "+req.params.actorId)
-    Actor.findOneAndUpdate({_id: req.params.actorId},  { $set: {"banned": "false" }}, {new: true, runValidators: true}, function(err, actor) {
-      if (err){
-        res.send(err);
-      }
-      else{
-        res.json(actor);
-      }
-    });
-  };
-
-exports.delete_an_actor = function(req, res) {
-    Actor.deleteOne({_id: req.params.actorId}, function(err, actor) {
-        if (err){
-            res.send(err);
-        }
-        else{
-            res.json({ message: 'Actor successfully deleted' });
-        }
-    });
+exports.ban_an_actor = function (req, res) {
+  //Check that the user is an Administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  console.log("banning an actor with id: " + req.params.actorId)
+  Actor.findOneAndUpdate({ _id: req.params.actorId }, { $set: { "banned": "true" } }, { new: true, runValidators: true }, function (err, actor) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json(actor);
+    }
+  });
 };
 
-exports.delete_all_actors = function(req, res) {
-    Actor.deleteMany({}, function(err, actor) {
-        if (err){
-            res.send(err);
-        }
-        else{
-            res.json({ message: 'All actors successfully deleted' });
-        }
-    });
+
+exports.unban_an_actor = function (req, res) {
+  //Check that the user is an Administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  console.log("banning an actor with id: " + req.params.actorId)
+  Actor.findOneAndUpdate({ _id: req.params.actorId }, { $set: { "banned": "false" } }, { new: true, runValidators: true }, function (err, actor) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json(actor);
+    }
+  });
+};
+
+exports.delete_an_actor = function (req, res) {
+  Actor.deleteOne({ _id: req.params.actorId }, function (err, actor) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json({ message: 'Actor successfully deleted' });
+    }
+  });
+};
+
+exports.delete_all_actors = function (req, res) {
+  Actor.deleteMany({}, function (err, actor) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json({ message: 'All actors successfully deleted' });
+    }
+  });
 };
